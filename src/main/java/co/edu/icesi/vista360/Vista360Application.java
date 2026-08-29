eb8f4284eb363cd73d295a3ebcb75da56514ee0e
Melo088 <jcml29310@gmail.com>
2026-08-29 16:38:17 -0500

    chore: scaffold Spring Boot con web, jpa, security, actuator and h2

---
+++ src/main/java/co/edu/icesi/vista360/Vista360Application.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.vista360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vista360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vista360Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/icesi/vista360/Vista360ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.vista360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vista360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
